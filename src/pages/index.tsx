028f7fc084af2af454d484ee3baaa84de895f5f8
ysndy <ysndy@naver.com>
2025-11-24 18:19:19 +0900

    Add Lottie animation and update primary color scheme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,19 +6,34 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
+import Lottie from 'react-lottie-player';
+import animationData from '@site/static/img/animation.json'; // Lottie JSON 경로
+
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
+
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
+      <div
+        className="container"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center', // 수평 중앙
+          justifyContent: 'center', // 수직 중앙
+          textAlign: 'center',     // 내부 텍스트 중앙
+          minHeight: '80vh',       // 화면의 거의 중앙에 위치
+        }}
+      >
         <Heading as="h1" className="hero__title">
-            <img 
-              src="./img/logogif.gif" 
-              alt="Logo" 
-              style={{ width: '500px', height: 'auto' }} 
-            />
+          <Lottie
+            loop
+            animationData={animationData}
+            play
+            style={{ width: 500, height: 500 }}
+          />
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
       </div>
