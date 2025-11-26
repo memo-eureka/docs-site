c08e7142b431ed653a7c2aabae2009b0f7d6d45d
ysndy <ysndy@naver.com>
2025-11-26 18:04:20 +0900

    Update social card image to Play Store icon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -70,7 +70,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/ic_launcher-playstore.png',
     colorMode: {
       respectPrefersColorScheme: true,
     },
